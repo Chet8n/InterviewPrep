f6473c3c9ed46dcca03182ec37f58a1cc0a24521
Chet8n <64730540+Chet8n@users.noreply.github.com>
2021-03-11 21:28:45 +0530

    Update Rain Water trapped.cpp

--- 1.Array and math/Rain Water trapped.cpp
+++ 1.Array and math/Rain Water trapped.cpp
@@ -3,24 +3,44 @@
 Problem : https://www.interviewbit.com/problems/rain-water-trapped/
 
 Rain Water Trapped
-Given n non-negative integers representing an elevation
-map where the width of each bar is 1,
-compute how much water it is able to trap after raining.
+Problem Description
+
+Given a vector A of non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it is able to trap after raining.
+
+Problem Constraints
+1 <= |A| <= 100000
 
 Input Format
-The only argument given is integer array A.
+First and only argument is the vector A
+
 Output Format
-Return the total water it is able to trap after raining..
+Return one integer, the answer to the question
 
-For Example
+Example Input
 Input 1:
-    A = [0,1,0,2,1,0,1,3,2,1,2,1]
+
+A = [0, 1, 0, 2]
+Input 2:
+
+A = [1, 2]
+
+
+Example Output
 Output 1:
-    6
-Explaination 1:
 
-    In this case, 6 units of rain water (blue section) are being trapped.
+1
+Output 2:
 
+0
+
+
+Example Explanation
+Explanation 1:
+
+1 unit is trapped on top of the 3rd element.
+Explanation 2:
+
+No water is trapped.
 */
 
 int Solution::trap(const vector<int> &a) {
