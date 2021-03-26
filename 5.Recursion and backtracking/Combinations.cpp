de09e9537ff08586caed157b8c4fe9b911f999c6
Chet8n <64730540+Chet8n@users.noreply.github.com>
2021-03-26 21:35:14 +0530

    Create Combinations.cpp

---
+++ 5.Recursion and backtracking/Combinations.cpp
@@ -0,0 +1,99 @@
+/*
+Combinations
+Problem Description
+
+Given two integers A and B, return all possible combinations of B numbers out of 1 2 3 ... A .
+
+Make sure the combinations are sorted.
+
+To elaborate,
+
+Within every entry, elements should be sorted. [1, 4] is a valid entry while [4, 1] is not.
+Entries should be sorted within themselves.
+WARNING: DO NOT USE LIBRARY FUNCTION FOR GENERATING COMBINATIONS.
+
+
+
+Problem Constraints
+1 <= A, B <= 10
+
+
+
+Input Format
+First argument is an integer A.
+Second argument is an integer B.
+
+
+
+Output Format
+Return a 2-D vector denoting all possible combinations.
+
+
+
+Example Input
+Input 1:
+
+ A = 4
+ B = 2
+Input 2:
+
+ A = 3
+ B = 2
+
+
+Example Output
+Output 1:
+
+ [
+  [1, 2],
+  [1, 3],
+  [1, 4],
+  [2, 3],
+  [2, 4],
+  [3, 4],
+ ]
+Output 2:
+
+ [
+  [1, 2],
+  [1, 3],
+  [2, 3]
+ ]
+
+
+Example Explanation
+Explanation 1:
+
+All the possible combinations of size 2 in sorted order.
+*/
+
+vector<vector<int> > Solution::combine(int a, int b) {
+
+    vector<vector<int>> ans;
+    vector<int> x;
+
+    auto go = [&](int ind, vector<int> &x, const auto & self) {
+
+        if (x.size() == b) {
+            ans.push_back(x);
+            return;
+        }
+
+        if (ind > a) {
+            return;
+        }
+
+        for (int i = ind; i <= a; i++) {
+            x.push_back(i);
+            self(i + 1, x, self);
+            x.pop_back();
+        }
+    };
+
+    go(1, x, go);
+
+    return ans;
+
+}
+
+
